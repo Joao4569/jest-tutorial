e42f929dac35145bc4d30e4811141315aaf42428
Joao4569 <johnnyrod4569@gmail.com>
2022-04-28 10:28:37 +0000

    End of tutorial

--- scripts/button.js
+++ scripts/button.js
@@ -2,9 +2,4 @@ function buttonClick() {
   document.getElementById("par").innerHTML = "You Clicked";
 }
 
-module.exports = async () => {
-  return {
-    verbose: true,
-  };
-};
 module.exports = buttonClick;
